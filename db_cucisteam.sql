ed1fa6662ea6e9f8cdb188acf7adf1e4aad46d4b
oktafiaanggraeni <oktafiaanggrnii@gmail.com>
2025-06-26 15:49:11 +0700

    update database

--- db_cucisteam.sql
+++ db_cucisteam.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 22 Jun 2025 pada 14.16
+-- Waktu pembuatan: 26 Jun 2025 pada 10.45
 -- Versi server: 10.4.28-MariaDB
 -- Versi PHP: 8.2.4
 
@@ -36,17 +36,18 @@ CREATE TABLE `data_karyawan` (
   `no_telp` varchar(15) DEFAULT NULL,
   `alamat` text DEFAULT NULL,
   `jabatan` varchar(50) DEFAULT NULL,
+  `shift` varchar(50) DEFAULT NULL,
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
-  `tanggal_masuk` date DEFAULT curdate()
+  `tanggal_masuk` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `data_karyawan`
 --
 
-INSERT INTO `data_karyawan` (`id_karyawan`, `nama_karyawan`, `jenis_kelamin`, `no_telp`, `alamat`, `jabatan`, `password`, `tanggal_masuk`) VALUES
-('K001', 'Alip', 'Laki-laki', '0987523', 'Indonesia', 'Admin', 'admin123', '2025-04-28'),
-('K002', 'Fardan', 'Laki-laki', '01234', 'Brazil', 'Washerman', 'karyawan123', '2025-04-30');
+INSERT INTO `data_karyawan` (`id_karyawan`, `nama_karyawan`, `jenis_kelamin`, `no_telp`, `alamat`, `jabatan`, `shift`, `password`, `tanggal_masuk`) VALUES
+('K001', 'Alip', 'Laki-laki', '0987523', 'Indonesia', 'Admin', 'Pagi', 'admin123', '2025-04-28'),
+('K002', 'bayu', 'Laki-laki', '01234', 'brazil', 'Washerman', 'Malam', 'karyawan123', '2025-04-30');
 
 -- --------------------------------------------------------
 
@@ -80,18 +81,23 @@ CREATE TABLE `data_pelanggan` (
   `id_pelanggan` varchar(10) NOT NULL,
   `nama_pelanggan` varchar(100) NOT NULL,
   `no_telp` varchar(15) DEFAULT NULL,
-  `alamat` text DEFAULT NULL
+  `alamat` text DEFAULT NULL,
+  `jenis_kendaraan` enum('Motor','Mobil') NOT NULL,
+  `model_kendaraan` varchar(50) DEFAULT NULL,
+  `jenis_layanan` varchar(50) DEFAULT NULL,
+  `plat_nomor` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `tanggal_cuci` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `data_pelanggan`
 --
 
-INSERT INTO `data_pelanggan` (`id_pelanggan`, `nama_pelanggan`, `no_telp`, `alamat`) VALUES
-('CS001', 'Bayu', '122345455', 'Indonesia'),
-('CS002', 'Ahmad', '012445', 'bogor'),
-('CS003', 'Ucok', '122345', 'fgfgfdgfgd'),
-('CS004', 'Arie', '12134343', 'dfsdfsd');
+INSERT INTO `data_pelanggan` (`id_pelanggan`, `nama_pelanggan`, `no_telp`, `alamat`, `jenis_kendaraan`, `model_kendaraan`, `jenis_layanan`, `plat_nomor`, `tanggal_cuci`) VALUES
+('CS001', 'Bayu', '122345455', 'Indonesia', 'Motor', 'Motor Besar', 'Cuci Motor Biasa', 'A 1234 BCD', '2025-04-28'),
+('CS002', 'nopal', '012445', 'bogor', 'Motor', 'Motor Kecil', 'Cuci Motor Premium', 'B 1222 BDE', '2025-04-30'),
+('CS003', 'sdad', '121123', 'asdada', 'Motor', 'Motor Besar', 'Cuci Motor Premium', 'B 121233 VDS', '2025-05-20'),
+('CS004', 'sdd', '12313', 'sdfsf', 'Mobil', 'Mobil Sedan', 'Cuci Mobil Premium', 'B 23123 DDS', '2025-05-20');
 
 -- --------------------------------------------------------
 
@@ -117,7 +123,7 @@ INSERT INTO `layanan_cuci` (`id_layanan`, `jenis_kendaraan`, `model_kendaraan`, 
 ('L002', 'motor', 'Motor Kecil', 'Cuci Motor Premium', '1. Semua layanan dari \"Cuci Motor Biasa\"\n2. Poles ringan bodi motor (plastik & tangki)\n3. Coating pelindung cat\n4. Pelumasan rantai (jika ada)\n5. Cuci dan pengharum helm', 25000.00),
 ('L003', 'motor', 'Motor Kecil', 'Cuci Motor Biasa', '1. Cuci seluruh bagian luar motor\n2. Pembersihan detail: sela bodi, \n3. kolong spakbor, velg\n4. Pembersihan rantai\n5. Semprotan air bertekanan tinggi\n6. Pengeringan dengan lap microfiber', 20000.00),
 ('L004', 'motor', 'Motor Besar', 'Cuci Motor Premium', '1. Semua layanan dari \"Cuci Motor Biasa\"\n2. Poles ringan bodi motor (plastik & tangki)\n3. Coating pelindung cat\n4. Pelumasan rantai (jika ada)\n5. Cuci dan pengharum helm', 30000.00),
-('L005', 'mobil', 'Mobil Sedan', 'Cuci Mobil Biasa', '1. Cuci seluruh bagian luar mobil (body, kaca, spion, kap, dan bumper)\n2. Pembersihan detail: sela-sela pintu, handle pintu, wiper\n3. Pembersihan kolong spakbor dan velg\n4. Pembersihan dasar eksterior grill dan emblem\n5. Semprotan air bertekanan tinggi (steam pressure washer)\n6. Vacuum interior: jok, lantai, dan bagasi\n7. Pengeringan dengan lap microfiber', 35000.00),
+('L005', 'mobil', 'Mobil Sedan', 'Cuci Motor Biasa', '1. Cuci seluruh bagian luar mobil (body, kaca, spion, kap, dan bumper)\n2. Pembersihan detail: sela-sela pintu, handle pintu, wiper\n3. Pembersihan kolong spakbor dan velg\n4. Pembersihan dasar eksterior grill dan emblem\n5. Semprotan air bertekanan tinggi (steam pressure washer)\n6. Vacuum interior: jok, lantai, dan bagasi\n7. Pengeringan dengan lap microfiber', 35000.00),
 ('L006', 'mobil', 'Mobil Sedan', 'Cuci Mobil Premium', '1. Semua layanan dari \"Cuci Mobil Biasa\"\n2. Poles ringan bodi mobil (terutama bagian plastik, krom, dan kap mesin)\n3. Coating pelindung cat sementara (wax atau sealant dasar)\n4. Pelumasan engsel pintu dan karet kaca (door trim)\n5. Semprotan parfum mobil (aroma segar atau sesuai pilihan)\n6. Semir ban dan poles velg ringan', 45000.00),
 ('L007', 'mobil', 'Mobil Hatchback', 'Cuci Mobil Biasa', '1. Cuci seluruh bagian luar mobil (body, kaca, spion, kap, dan bumper)\n2. Pembersihan detail: sela-sela pintu, handle pintu, wiper\n3. Pembersihan kolong spakbor dan velg\n4. Pembersihan dasar eksterior grill dan emblem\n5. Semprotan air bertekanan tinggi (steam pressure washer)\n6. Vacuum interior: jok, lantai, dan bagasi\n7. Pengeringan dengan lap microfiber', 40000.00),
 ('L008', 'mobil', 'Mobil Hatchback', 'Cuci Mobil Premium', '1. Semua layanan dari \"Cuci Mobil Biasa\"\n2. Poles ringan bodi mobil (terutama bagian plastik, krom, dan kap mesin)\n3. Coating pelindung cat sementara (wax atau sealant dasar)\n4. Pelumasan engsel pintu dan karet kaca (door trim)\n5. Semprotan parfum mobil (aroma segar atau sesuai pilihan)\n6. Semir ban dan poles velg ringan', 50000.00),
@@ -131,30 +137,23 @@ INSERT INTO `layanan_cuci` (`id_layanan`, `jenis_kendaraan`, `model_kendaraan`, 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `transaksi_cuci`
+-- Struktur dari tabel `shift`
 --
 
-CREATE TABLE `transaksi_cuci` (
-  `id_transaksi` varchar(10) NOT NULL,
-  `tanggal_transaksi` date NOT NULL,
-  `id_karyawan` varchar(10) NOT NULL,
-  `id_pelanggan` varchar(10) NOT NULL,
-  `id_layanan` varchar(10) NOT NULL,
-  `harga` decimal(10,2) NOT NULL,
-  `diskon` int(11) DEFAULT 0,
-  `metode_pembayaran` varchar(20) NOT NULL
+CREATE TABLE `shift` (
+  `id_shift` varchar(10) NOT NULL,
+  `shift` enum('Pagi','Malam') DEFAULT NULL,
+  `jam_mulai` time DEFAULT NULL,
+  `jam_selesai` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `transaksi_cuci`
+-- Dumping data untuk tabel `shift`
 --
 
-INSERT INTO `transaksi_cuci` (`id_transaksi`, `tanggal_transaksi`, `id_karyawan`, `id_pelanggan`, `id_layanan`, `harga`, `diskon`, `metode_pembayaran`) VALUES
-('TRX001', '2025-05-20', 'K001', 'CS001', 'L001', 15000.00, 0, 'E-Wallet'),
-('TRX002', '2025-05-28', 'K002', 'CS002', 'L002', 25000.00, 0, 'Tunai'),
-('TRX003', '2025-05-30', 'K001', 'CS002', 'L002', 25000.00, 0, 'Transfer'),
-('TRX004', '2025-06-21', 'K002', 'CS002', 'L002', 25000.00, 0, 'Tunai'),
-('TRX005', '2025-06-21', 'K001', 'CS003', 'L006', 45000.00, 0, 'Transfer');
+INSERT INTO `shift` (`id_shift`, `shift`, `jam_mulai`, `jam_selesai`) VALUES
+('S01', 'Pagi', '08:00:00', '15:00:00'),
+('S02', 'Malam', '15:00:00', '21:00:00');
 
 --
 -- Indexes for dumped tables
@@ -165,13 +164,6 @@ INSERT INTO `transaksi_cuci` (`id_transaksi`, `tanggal_transaksi`, `id_karyawan`
 --
 ALTER TABLE `data_karyawan`
   ADD PRIMARY KEY (`id_karyawan`);
-
---
--- Indeks untuk tabel `data_kendaraan`
---
-ALTER TABLE `data_kendaraan`
-  ADD PRIMARY KEY (`id_kendaraan`),
-  ADD KEY `id_pelanggan` (`id_pelanggan`);
 
 --
 -- Indeks untuk tabel `data_pelanggan`
@@ -186,31 +178,10 @@ ALTER TABLE `layanan_cuci`
   ADD PRIMARY KEY (`id_layanan`);
 
 --
--- Indeks untuk tabel `transaksi_cuci`
+-- Indeks untuk tabel `shift`
 --
-ALTER TABLE `transaksi_cuci`
-  ADD PRIMARY KEY (`id_transaksi`),
-  ADD KEY `id_pelanggan` (`id_pelanggan`),
-  ADD KEY `id_karyawan` (`id_karyawan`),
-  ADD KEY `id_layanan` (`id_layanan`);
-
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `data_kendaraan`
---
-ALTER TABLE `data_kendaraan`
-  ADD CONSTRAINT `data_kendaraan_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `data_pelanggan` (`id_pelanggan`);
-
---
--- Ketidakleluasaan untuk tabel `transaksi_cuci`
---
-ALTER TABLE `transaksi_cuci`
-  ADD CONSTRAINT `transaksi_cuci_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `data_pelanggan` (`id_pelanggan`),
-  ADD CONSTRAINT `transaksi_cuci_ibfk_2` FOREIGN KEY (`id_karyawan`) REFERENCES `data_karyawan` (`id_karyawan`),
-  ADD CONSTRAINT `transaksi_cuci_ibfk_3` FOREIGN KEY (`id_layanan`) REFERENCES `layanan_cuci` (`id_layanan`);
+ALTER TABLE `shift`
+  ADD PRIMARY KEY (`id_shift`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
